1b9c6f4fd6b041d2784d786486f4d15b56d04685
dianaasss <diana28apr2005@gmail.com>
2025-11-27 02:08:36 +0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,13 @@
+class Book:
+    material = 'paper'
+    cover = 'paperback'
+    all_books = []
+
+# экземпляр класса Book
+my_book = Book()
+
+
+print(my_book.material)
+print(my_book.cover)
+print(my_book.all_books)
+
